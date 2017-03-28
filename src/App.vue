d935592bbe3cb2264cf15347059dae9cdf57a966
Xuning Tian <xtian513@gmail.com>
2017-03-28 16:13:30 +0800

    app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <a @click.prevent="clickLinkOne">Go to Hello</a>
-    <a @click.prevent="clickLinkTwo">Go to Test</a>
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <a @click.prevent="clickLinkOne">Go to Hello</a>
+    <a @click.prevent="clickLinkTwo">Go to Test</a> -->
+    <div class="touchWrap" @touchstart='vTouchStart'>
+      
+        <viking-home></viking-home>
+
+    </div>
     <router-view></router-view>
+    
   </div>
 </template>
 
 <script>
+import vikingHome from '@/components/viking-home'
 export default {
   name: 'app',
   methods: {
@@ -16,22 +23,38 @@ export default {
     },
     clickLinkTwo(){
       this.$router.push('/test')
-    }
+    },
+    vTouchStart: function(e){
+      alert(1);
+    },
+
+  },
+  components:{
+    vikingHome
   }
 }
 </script>
 
 <style lang="scss">
+html,body {
+  height: 100%;
+  background-size: cover;
+}
 $color: red;
 $fontsize:1.8rem;
 #app {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
-  background: $color;
   font-size: $fontsize;
+  height: 100%;
+  .touchWrap {
+    
+  }
 }
 </style>
