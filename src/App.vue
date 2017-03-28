1a871d74e07977e9e904b7e70146c73ca2eabbbd
Xuning Tian <xtian513@gmail.com>
2017-03-28 10:25:08 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <a @click.prevent="clickLinkOne">Go to Hello</a>
+    <a @click.prevent="clickLinkTwo">Go to Test</a>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  methods: {
+    clickLinkOne(){
+      this.$router.push('/')
+    },
+    clickLinkTwo(){
+      this.$router.push('/test')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
