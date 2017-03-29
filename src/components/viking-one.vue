f003289e4b26bcea04bdbcb106e79faa4714ee38
qipeijun <qipeijun@aliyun.com>
2017-03-29 10:49:25 +0800

    提交one

--- src/components/viking-one.vue
+++ src/components/viking-one.vue
@@ -1,25 +1,115 @@
 <template>
   <div class="viking-one">
     <div class="one-outer">
-      <div class="one">
-        <h3 class="vk-title">巡游人文欧洲 &nbsp;体验内河游轮之魅</h3>
-        <p class="vk-text">河流是人类文明的摇篮</p>
-        <p class="vk-text">每一条河流 &nbsp;都有着自己的故事</p>
-        <p class="vk-text">一如欧洲的莱茵河与多瑙河</p>
-        <p class="vk-text">蜿蜒于欧洲诸国</p>
-        <p class="vk-text">流经之地 &nbsp;即是欧洲文明的腹地</p>
-        <p class="vk-text">维京内河游轮 &nbsp;航行于这两条璀璨流域</p>
-        <p class="vk-text">更穿梭于河流两岸的人文之美</p>
-        <p class="vk-text">将宏伟教堂 &nbsp;古老建筑</p>
-        <p class="vk-text">摩登艺术中心和璀璨的都市生活</p>
-        <p class="vk-text">呈现于您的眼前</p>
-        <div class="video-box">
-          <!-- <span style="display: block;width: 100%;height: 1px;background-color: #cacaca"></span> -->
-          <video id="mp4" :src="firstLink" controls></video>
+      <div class="container" style="left:0">
+      <!-- 第一屏 -->
+        <div class="one-wrap">
+          <div class="white white-left"></div>
+          <div class="white white-right"></div>
+          <div class="one oneLeft left">
+            <h3 class="vk-title">维京荣耀 &nbsp;屡获殊荣</h3>
+            <p class="vk-text">旗下46艘维京船自2012年起屡获殊荣</p>
+            <div class="line3"></div>
+            <div class="glory glory1">
+              <div>
+                <p class="vk-text4">11次荣膺 Travel + Leisure</p>
+                <p class="vk-text4 last">World’s Best Awards  全球最佳游轮公司</p>
+              </div>
+            </div>
+            <div class="glory glory2"> 
+              <div>
+                <p class="vk-text4">10次荣膺 Condé Nast Traveler</p>
+                <p class="vk-text4 last">Readers’ Choice Awards  最佳河轮</p>
+              </div>
+            </div>
+            <div class="glory glory3">
+              <div>
+                <p class="vk-text4">5次荣膺  Cruise Critic</p>
+                <p class="vk-text4 last">Editor’s Pick</p>
+              </div>
+            </div>
+            <div class="glory glory4">
+              <div>
+                <p class="vk-text4">10次荣膺 Travel Weekly</p>
+                <p class="vk-text4 last">Readers’ Choice Awards 最佳游轮公司</p>
+              </div>
+            </div>
+            <div class="glory glory5">
+              <div>
+                <p class="vk-text4">6 次荣膺</p>
+                <p class="vk-text4">British Travel Awards</p>
+                <p class="vk-text4 last">最佳游轮公司</p>
+              </div>
+            </div>
+            <div class="glory glory6">
+              <div>
+                <p class="vk-text4">10 次荣膺</p>
+                <p class="vk-text4">Recommend Readers’ Choice Awards</p>
+                <p style="margin-bottom: .1rem" class="vk-text4 last">最佳游轮公司</p>
+                <p class="vk-text4">......</p>
+              </div>
+            </div>
+          </div>
         </div>
-        <p class="more" @click="xclick">更多视频<span class="more-icon"></span></p>
-        <div class="line"></div>
+        <!-- 第二屏 -->
+        <div class="one-wrap">
+          <div class="white white-left"></div>
+          <div class="white white-right"></div>
+          <div class="one oneCenter center">
+            <h3 class="vk-title">巡游人文欧洲 &nbsp;体验内河游轮之魅</h3>
+            <p class="vk-text">河流是人类文明的摇篮</p>
+            <p class="vk-text">每一条河流 &nbsp;都有着自己的故事</p>
+            <p class="vk-text">一如欧洲的莱茵河与多瑙河</p>
+            <p class="vk-text">蜿蜒于欧洲诸国</p>
+            <p class="vk-text">流经之地 &nbsp;即是欧洲文明的腹地</p>
+            <p class="vk-text">维京内河游轮 &nbsp;航行于这两条璀璨流域</p>
+            <p class="vk-text">更穿梭于河流两岸的人文之美</p>
+            <p class="vk-text">将宏伟教堂 &nbsp;古老建筑</p>
+            <p class="vk-text">摩登艺术中心和璀璨的都市生活</p>
+            <p class="vk-text">呈现于您的眼前</p>
+            <div class="video-box">
+              <!-- <span style="display: block;width: 100%;height: 1px;background-color: #cacaca"></span> -->
+              <video id="mp4" :src="firstLink" controls webkit-playsinline :poster="imgurl"></video>
+            </div>
+            <p class="more">更多视频<span class="more-icon"></span></p>
+            <div class="line"></div>
+            <div class="video-wrap">
+              <ul :style="{width:Allwidth}" style="left:0">
+                <li class="xvideo" v-for="item in videoData">
+                  <div class="videosm">
+                    <video :src="item.link" controls :poster="imgurl" webkit-playsinline></video>
+                  </div>
+                  <p class="video-title">{{item.title}}</p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <!-- 第三屏 -->
+        <div class="one-wrap">
+          <div class="white white-left"></div>
+          <div class="white white-right"></div>
+          <div class="one oneRight right">
+            <h3 class="vk-title">维京游轮 &nbsp;游轮界的劳斯莱斯</h3>
+            <p class="vk-text">旨在为全球旅行者提供高品质的内河与海洋游轮服务</p>
+            <img class="pic1" src="../assets/img/one/pic1.png" height="404" width="568" alt="">
+            <p class="vk-text1">优先入港 &nbsp;量身定制</p>
+            <p class="vk-text2">维京邮轮拥有62艘内河游轮</p>
+            <p class="vk-text2">4艘海洋游轮 超过120个停靠口岸</p>
+            <p class="vk-text2">累计接待38万游客 &nbsp;&nbsp;340万游客累积巡航天数</p>
+            <div class="line1"></div>
+            <p class="vk-text3"><span> &nbsp;*&nbsp;</span>作为市场领导者维京游轮可优先进入城区各主要景点2016年推出首批为中国游客量身打造的欧洲内河游轮航线</p>
+          </div>
+        </div>
+        
+        <!-- 小圆点 -->
+        
       </div>
+      <ul class="dot-wrap">
+          <li :class="{'active':isCards == 0}"></li>
+          <li :class="{'active':isCards == 1}"></li>
+          <li :class="{'active':isCards == 2}"></li>
+        </ul>
     </div>
   </div>
 </template>
@@ -27,7 +117,7 @@
 export default {
   data() {
       return {
-        msg: 'viking-one.vue',
+        // videoData:[],
         videoData: [{
           'title': 'Vikinfg<游轮带你去旅行>主题宣传片1111',
           'link': 'http://image.linbaoyou.com/upload/test/video/2017032020302405.mp4'
@@ -44,21 +134,100 @@ export default {
           'title': 'Vikinfg<游轮带你去旅行>主题宣传片3333',
           'link': 'http://image.linbaoyou.com/upload/test/video/20170320194127900.mp4'
         }, ],
-        firstLink: ""
+        firstLink: "",
+        Allwidth: '',
+        imgurl: require('./logo.png'),
+        isCards:0,
       }
     },
     methods: {
-    	xclick(){
-    		console.log(this.firstLink);
-    	}
+
     },
-    computed: {
+    mounted() {
+      this.firstLink = this.videoData[0].link;
+      this.Allwidth = this.videoData.length * (1.83 + 0.1) - 0.1 + "rem"; //更多视频总长度
+
+      // ==========================  滑动   ================================================
+      var self = this;
+      var xstar, ystar, xmove, ymove, xend, yend, disx;
+      document.addEventListener('touchstart', function(e) {
+
+        xstar = e.touches[0].pageX;
+        ystar = e.touches[0].pageX;
+
+
+      }, false);
+      document.addEventListener('touchmove', function(e) {
+      	e.preventDefault()
+        xmove = e.touches[0].pageX;
+        ymove = e.touches[0].pageX;
+        var disx = xmove-xstar
+        console.log(disx);
+        document.querySelector('.container').style.left = disx/100+parseInt(document.querySelector('.container').style.left)/100 +'rem'
+
+      }, false);
+
+      document.addEventListener('touchend', function(e) {
+        xend = e.changedTouches[0].pageX;
+        yend = e.changedTouches[0].pageY;
+        if (xstar - xend < -45) { //右滑动
+        	 //小圆点
+          self.isCards--
+          if (self.isCards<=0) {
+          	self.isCards = 0
+          }
+        	console.log('右滑动');
+          document.querySelector('.container').style.left = -7.5*self.isCards +'rem'
+
+         
+        } else if (xstar - xend > 45) { //左滑动
+        	 //小圆点
+          self.isCards++
+          if (self.isCards>=2) {
+          	self.isCards = 2
+          }
+        	console.log('左滑动');
+          document.querySelector('.container').style.left = -7.5*self.isCards +'rem'
+
+         
+        } else if (xstar - xend >= -45 && xstar - xend < 0) { //不满足右滑动
+          if (xend < 0) {
+            console.log('满足右滑动');
+          } else {
+            console.log('test');
+          }
+        } else if (xstar - xend <= 45 && xstar - xend > 0) { //不满足左滑动
+          if (xend > 636) {
+            console.log('不满足左滑动');
+          } else {
+            console.log('test');
+          }
+
+        } else {
+          return
+        }
+        //清除事件监听
+        document.removeEventListener('touchstart', function() {})
+        document.removeEventListener('touchmove', function() {})
+        document.removeEventListener('touchend', function() {})
+      }, false);
+
+
 
 
     },
     ready() {
-    		this.firstLink = this.videoData[0].link
+
     },
+    created: function() {
+      //ajax请求数据
+      // this.$http.get('/liner/liner_getLinerVideo').then(function(res) {
+      //   this.videoData = JSON.parse(res.data);
+      // }, function(err) {});
+
+
+    },
+
 }
 </script>
 <style>
@@ -80,6 +249,42 @@ export default {
   width: 100%;
   height: 100%;
   padding-top: .41rem;
+  position: relative;
+  overflow: hidden;
+}
+
+.container {
+  width: 22.5rem;
+  height: 9.66rem;
+  position: absolute;
+  left: 0;
+  transition: all .3s ease;
+}
+
+.one-wrap {
+  width: 7.5rem;
+  position: relative;
+  float: left;
+}
+
+.one-wrap .white {
+  width: .24rem;
+  height: 7.95rem;
+  background-color: rgba(255, 255, 255, .5);
+  position: absolute;
+  top: .85rem;
+}
+
+.one-wrap .white.white-left {
+  left: 0;
+  border-top-right-radius: .07rem;
+  border-bottom-right-radius: .07rem;
+}
+
+.one-wrap .white.white-right {
+  right: 0;
+  border-top-left-radius: .07rem;
+  border-bottom-left-radius: .07rem;
 }
 
 .one {
@@ -89,6 +294,15 @@ export default {
   border-radius: .08rem;
   margin: 0 auto;
   padding-top: .29rem;
+  box-shadow: 0 0 10px rgba(0, 0, 0, .3);
+}
+
+.left {
+  /* display: none */
+}
+
+.right {
+  /* display: none */
 }
 
 .vk-title {
@@ -143,5 +357,193 @@ export default {
   margin: 0 auto;
   background-color: #3b3b3b;
   margin-bottom: .09rem;
+}
+
+.video-wrap {
+  width: 5.69rem;
+  height: 1.4rem;
+  margin: 0 auto;
+  position: relative;
+  overflow: auto;
+  transition: all .1s ease;
+}
+
+.video-wrap ul {
+  position: absolute;
+}
+
+.xvideo {
+  width: 1.83rem;
+  float: left;
+}
+
+.xvideo + .xvideo {
+  margin-left: .1rem;
+}
+
+.xvideo .videosm {
+  width: 100%;
+  height: 1.02rem;
+  background-color: #fff;
+  border: 4px solid #fff;
+  margin-bottom: .09rem;
+  box-shadow: 1px 0px 4px rgba(0, 0, 0, .05), -1px 0px 4px rgba(0, 0, 0, .05), 0px 4px 10px rgba(0, 0, 0, .2);
+}
+
+.xvideo .videosm video {
+  width: 1.75rem;
+  height: .94rem;
+}
+
+.video-title {
+  display: block;
+  width: 1.83rem;
+  height: .17rem;
+  line-height: .17rem;
+  font-size: .17rem;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+
+.dot-wrap {
+  height: .16rem;
+  position: absolute;
+  left: 50%;
+  top: 10.28rem;
+  transform: translateX(-50%);
+  transition: all .2s ease;
+}
+
+.dot-wrap li {
+  float: left;
+  width: .16rem;
+  height: .16rem;
+  margin: 0 .05rem;
+  border-radius: .08rem;
+  border: 2px solid #b81b48;
+}
+
+.dot-wrap li.active {
+  background-color: #b81b48;
+  width: .32rem;
+}
+
+.oneRight {
+  padding-top: .36rem;
+}
+
+.oneRight .pic1 {
+  display: block;
+  margin: .29rem auto .6rem;
+}
+
+.oneRight .vk-text1 {
+  height: .29rem;
+  font-size: .29rem;
+  color: #323232;
+  line-height: .29rem;
+  text-align: center;
+  margin-bottom: .34rem;
+  font-weight: 600;
+}
+
+.oneRight .vk-text2 {
+  height: .22rem;
+  font-size: .22rem;
+  color: #3f3f3f;
+  line-height: .22rem;
+  text-align: center;
+  margin-bottom: .13rem;
+}
+
+.line1 {
+  margin: 0 auto;
+  margin-top: .56rem;
+  margin-bottom: .31rem;
+  height: .01rem;
+  background-color: #dedede;
+  width: 5.68rem;
+}
+
+.oneRight .vk-text3 {
+  padding: 0 .27rem;
+  font-size: .21rem;
+  color: #8a8a8a;
+  line-height: .32rem;
+}
+
+.oneRight .vk-text3 span {
+  font-size: .21rem;
+  color: #cf1747;
+  line-height: .32rem;
+}
+
+.oneLeft .vk-title {
+  margin-bottom: .15rem;
+}
+
+.oneLeft .vk-text {
+  margin-bottom: .2rem;
+}
+
+.oneLeft .line3 {
+  width: 100%;
+  height: .18rem;
+  background: url('../assets/img/one/lineLeft.png') no-repeat;
+  background-size: 100% 100%;
+  margin-bottom: .16rem;
+}
+
+.glory {
+  width: 5.29rem;
+  margin-right: .31rem;
+  float: right;
+  padding-left: 1.49rem;
+  padding-top: .33rem;
+}
+
+.glory >div {
+  width: 100%;
+  border-bottom: 1px solid #dedede
+}
+
+.glory >div .vk-text4 {
+  font-size: .19rem;
+  color: #565555;
+  margin-bottom: .1rem;
+}
+
+.glory >div .vk-text4.last {
+  margin-bottom: .35rem;
+}
+
+.glory1 {
+  background: url('../assets/img/one/logo_1.png') left top no-repeat;
+}
+
+.glory2 {
+  background: url('../assets/img/one/logo_2.png') left -.05rem no-repeat;
+}
+
+.glory3 {
+  background: url('../assets/img/one/logo_3.png') left -.08rem no-repeat;
+}
+
+.glory4 {
+  background: url('../assets/img/one/logo_4.png') left -.1rem no-repeat;
+}
+
+.glory5 {
+  background: url('../assets/img/one/logo_05.png') left top no-repeat;
+}
+
+.glory6 {
+  background: url('../assets/img/one/logo_06.png') left top no-repeat;
+}
+
+.glory6 >div {
+  border-bottom: none;
+  position: relative;
 }
 </style>
