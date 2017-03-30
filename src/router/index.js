a5e752ea16ea848bc9c1bc760988e1cba1923762
Xuning Tian <xtian513@gmail.com>
2017-03-30 19:54:13 +0800

    ajax

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Test from '@/components/test'
+import vikingHome from '@/components/viking-home'
+import vikingOne from '@/components/viking-one'
+import vikingTwo from '@/components/viking-two'
+import vikingThree from '@/components/viking-three'
+import vikingFour from '@/components/viking-four'
 
 Vue.use(Router)
 
@@ -9,9 +14,25 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'vikingOne',
+      component: vikingOne
     },
+    {
+      path: '/vikingTwo',
+      name: 'vikingTwo',
+      component: vikingTwo
+    },
+    {
+      path: '/vikingThree',
+      name: 'vikingThree',
+      component: vikingThree
+    },
+    {
+      path: '/vikingFour',
+      name: 'vikingFour',
+      component: vikingFour
+    },
+    
     {
       path: '/test',
       name: 'Test',
